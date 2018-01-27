1caa8eee1fdec9131adef710cc6f3c7d2c1fa7d4
Murilo Costa <murilo.costa@tapps.com.br>
2018-01-27 19:17:12 -0200

    Menu Scenes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+	public static int currentStage = 1;
+
+	public static float currentScore;
+
+	public static int totalStages = 5;
+
+	// Use this for initialization
+	void Start () {
+		ResetScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void ResetScore() {
+		currentScore = 0f;
+	}
+
+	public static void PlayCurrentStage() {
+		SceneManager.LoadScene("Scenes/Stages/Stage" + currentStage);
+		ResetScore();
+	}
+}
---
+++ Assets/Scripts/LoseGame.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoseGame : MonoBehaviour {
+
+	public Text gameOverText;
+
+	// Use this for initialization
+	void Start () {
+		gameOverText.text = "Game Over!\nYour Score: " + GameManager.currentScore;
+		transform.Find("RetryButton").GetComponent<Button>().onClick.AddListener(OnClickRetry);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnClickRetry() {
+		GameManager.PlayCurrentStage();
+	}
+
+	void OnClickMenu() {
+		SceneManager.LoadScene("Scenes/Menus/Menu");
+	}
+}
